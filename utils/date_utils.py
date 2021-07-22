48994afb1a06ed6b23e8dbd3f50a5385f970e5b3
Pamilerin Idowu <pamilerinid@gmail.com>
2021-07-22 19:33:33 +0100

    endpoint create

---
+++ utils/date_utils.py
@@ -0,0 +1,5 @@
+def to_date(date_string): 
+    try:
+        return datetime.datetime.strptime(date_string, "%Y-%m-%d").date()
+    except ValueError:
+        raise ValueError('{} is not valid date in the format YYYY-MM-DD'.format(date_string))
